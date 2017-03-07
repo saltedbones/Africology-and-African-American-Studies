32d060db8020c6408b08d8fbd5362eaa48d56d0d
Victor Lourng <hello@victorlourng.com>
2017-03-07 17:05:23 -0500

    [batch] Move feed files out of repo and into prebuild script

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 git pull
 bundle update --source tutheme
+wget -O ./_data/news.csv $(bundle exec yaml get news-feed -F=_config.yml)
+wget -O ./_data/events.csv $(bundle exec yaml get events-feed -F=_config.yml)
+wget -O ./_data/profiles.csv $(bundle exec yaml get faculty-feed -F=_config.yml)
 bundle exec jekyll serve --watch --drafts
